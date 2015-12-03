e1f40e57c4503d1a82a6dd65bcc58ccce7243cde
Sophia Ngo <sophia.ngo@sjsu.edu>
2015-12-02 19:45:22 -0800

    fixed default image

--- config/passport.js
+++ config/passport.js
@@ -8,6 +8,11 @@ var User = require('../models/user');
 // load the auth variables
 var configAuth = require('./auth');
 
+var fb_default_male_img = 'https://scontent.xx.fbcdn.net/hprofile-xfa1/v/t1.0-1/s200x200/10354686_10150004552801856_220367501106153455_n.jpg?oh=3d28110b4889284a44dcf3b9a346d99d&oe=56E86650';
+var fb_default_female_img = 'https://scontent.xx.fbcdn.net/hprofile-xla1/v/t1.0-1/s200x200/1379841_10150004552801901_469209496895221757_n.jpg?oh=3fa29a859650d406793272ee7cff2a64&oe=56EFB3F8';
+var vwic_default_female_img = 'https://api.icons8.com/download/394ff97750654c24ef003d36092b3dafa4679e17/Color/PNG/144/Very_Basic/user_female-144.png';
+var vwic_default_male_img = 'https://api.icons8.com/download/4ef7a2fc605db06baac2320c471f9fde67cd0209/Color/PNG/144/Very_Basic/user_male-144.png';
+
 // expose this function to our app using module.exports
 module.exports = function(passport) {
 
@@ -63,15 +68,22 @@ module.exports = function(passport) {
             return done(null, user);
           });
         } else {
-          console.log('data: ' + JSON.stringify(profile));
+          // console.log('data: ' + JSON.stringify(profile));
           // if there is no user found with that facebook id, create them
+
           var newUser = new User();
           // set all of the facebook information in our user model
           newUser.user_id = profile.id; // set the users facebook id
           newUser.token = token; // we will save the token that facebook provides to the user
           newUser.displayName = profile.displayName;
           newUser.fullname = profile.name.givenName + ' ' + profile.name.familyName;
-          newUser.imageUrl = profile.photos[0].value;
+          if (profile.photos[0].value===fb_default_female_img){
+            newUser.imageUrl = vwic_default_female_img;
+          } else if (profile.photos[0].value===fb_default_male_img){
+            newUser.imageUrl = vwic_default_male_img;
+          } else {
+            newUser.imageUrl = profile.photos[0].value;
+          }
           newUser.gender = profile.gender;
           newUser.email = profile.emails[0].value;
           // save our user to the database
