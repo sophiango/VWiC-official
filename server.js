b964991fddc7c68726451321f47510e050295396
Sophia Ngo <sophia.ngo@sjsu.edu>
2015-12-22 22:07:29 +0700

    change port

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ var server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
- var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
  var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
  server.listen(server_port, server_ip_address, function () {
