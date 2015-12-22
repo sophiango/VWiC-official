afca5e2a08e8f23003f1574ffd3af2742a6d391d
Sophia Ngo <sophia.ngo@sjsu.edu>
2015-12-22 22:21:32 +0700

    change port

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ var server = http.createServer(app);
  var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
  server.listen(server_port, server_ip_address, function () {
-   console.log( "Listening on " + server_ip_address + ", server_port " + port )
+   console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
  });
 
 module.exports = app;
